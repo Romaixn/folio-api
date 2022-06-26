5ffe17db22557af00bbbab1c7d96a67117edefd2
Romain <romain.herault99@gmail.com>
2022-06-26 21:08:57 +0200

    feat: adding base login and dashboard templates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./templates/**/*.html.twig'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
