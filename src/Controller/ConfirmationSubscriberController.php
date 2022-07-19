4ae5ac053596fce4545e189cf2edca1f72270c0c
Romain <romain.herault99@gmail.com>
2022-07-19 14:15:59 +0200

    fix: verified subscribers

--- src/Controller/ConfirmationSubscriberController.php
+++ src/Controller/ConfirmationSubscriberController.php
@@ -3,16 +3,17 @@ namespace App\Controller;
 
 use App\Entity\Subscriber;
 use App\Repository\SubscriberRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class ConfirmationSubscriberController extends AbstractController
 {
     #[Route('/confirmation/subscriber/{id}', name:'confirmation_subscriber')]
-    public function __invoke(Subscriber $subscriber, SubscriberRepository $subscriberRepository)
+    public function __invoke(Subscriber $subscriber, EntityManagerInterface $entityManager)
     {
         $subscriber->setIsVerified(true);
-        $subscriberRepository->add($subscriber, true);
+        $entityManager->flush();
 
         return $this->redirect('https://rherault.fr/confirmation');
     }
